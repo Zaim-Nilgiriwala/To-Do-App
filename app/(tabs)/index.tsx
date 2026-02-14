6fe756669f10fc75824fd4355b60ad9184285d14
Zaim-Nilgiriwala <Zaimnilg@gmail.com>
2026-02-14 17:29:07 -0600

    Set up working backend

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,10 +3,11 @@ import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const { toggleDarkMode } = useTheme();
+
+
   return (
     <View
-      style={styles.container}
-    >
+      style={styles.container}>
       <Text style={styles.content}>Edit app/index.tsx to edit this screen.</Text>
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>toggle the mode</Text>
