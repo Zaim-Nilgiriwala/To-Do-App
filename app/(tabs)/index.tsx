7a8890f166bc97e24b6b57805d5f8ca692ce1ada
Zaim-Nilgiriwala <Zaimnilg@gmail.com>
2026-02-14 19:34:59 -0600

    added styles and header

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,28 +1,25 @@
+import { createHomeStyles } from "@/assets/styles/home.styles";
+import Header from "@/components/Header";
 import useTheme from "@/hooks/useTheme";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-  const { toggleDarkMode } = useTheme();
+  const { toggleDarkMode, colors } = useTheme();
 
-
+  const homeStyles = createHomeStyles(colors);
+  
   return (
-    <View
-      style={styles.container}>
-      <Text style={styles.content}>Edit app/index.tsx to edit this screen.</Text>
+      <LinearGradient colors={colors.gradients.background} style={homeStyles.container}>
+        <StatusBar barStyle= {colors.statusBarStyle} />
+    <SafeAreaView style={homeStyles.safeArea}>
+      <Header />
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>toggle the mode</Text>
       </TouchableOpacity>
-    </View>
+    </SafeAreaView>
+      </LinearGradient>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      },
-  content: {
-        fontSize: 18,
-      },
-});
